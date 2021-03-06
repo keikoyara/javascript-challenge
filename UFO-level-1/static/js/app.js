f0cb1dc759240393233aa46b71cca8a06ea1503d
Keiko Yara <Keiko@Keikos-MBP.fios-router.home>
2021-03-06 18:49:57 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,9 +3,25 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-// Get references to tbody
-var tbody = d3.select("tbody");
+// Get references to emelemnts: tbody, input filed and button
+var tbody = document.querySelector("tbody");
+var dateInput = document.querySelector('#datetime');
+var searchBtn = document.querySelector('#filter-btn');
 
-for(var i = 0; i < tableData.length; i++) {
-    // addint comment
-}
+function renderTable(){
+    tbody.innerHTML= "";
+
+    for(var i = 0; i < tableData.length; i++) {
+        // grab current data object and fields
+        var data = tableData[i];
+        var fields = Object.keys(data);
+        // create new row and set index 
+        var row = tbody.insertRow(i);
+        for (var j = 0; j < fields.length; j++) {
+            var field = fields[j];
+            var cell = row.insertCell(j);
+            cell.innerText = data[field];
+        }
+    };
+};
+
